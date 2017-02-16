65626b590162e47e5f661dddec9586656032c7f5
yukemin <yukemin@jd.com>
2017-02-16 20:19:26 +0800

    add SpringTestNG

---
+++ src/test/java/com/yu/testng/SpringTestNG.java
@@ -0,0 +1,41 @@
+package com.yu.testng;
+ 
+import org.testng.Assert;
+import org.springframework.beans.factory.annotation.Autowired;  
+import org.springframework.test.context.ContextConfiguration;  
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;  
+import org.testng.annotations.AfterClass;  
+import org.testng.annotations.BeforeClass;  
+import org.testng.annotations.Test;
+
+import com.yu.domain.User;
+import com.yu.service.UserService;  
+
+@ContextConfiguration(locations = { "classpath*:/application.xml" })  
+public class SpringTestNG extends AbstractTestNGSpringContextTests {     
+      
+	@Autowired  
+    private UserService userService; 
+	
+    @BeforeClass  
+    public void setUp(){  
+        System.out.println("setup()");  
+    }  
+      
+    @Test  
+    public void testOk() {    
+        Assert.assertTrue(1 == 1);
+    }  
+    
+	@Test  
+    public void selectUserByIdTest(){  
+		User user = userService.selectUserById(1);  
+		System.out.println(user.getUserName() + ":" + user.getUserPassword());
+		Assert.assertEquals("luoguohui", user.getUserName());
+	} 
+      
+    @AfterClass  
+    public void destroy() {  
+        System.out.println("exit()");  
+    }  
+}  
--- src/test/java/com/yu/testng/UserServiceTestNG.java
+++ src/test/java/com/yu/testng/UserServiceTestNG.java
@@ -5,16 +5,22 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
 import org.testng.Assert;
-import com.yu.domain.User;
-import com.yu.service.UserService;
+//import com.yu.domain.User;
+//import com.yu.service.UserService;
+import org.testng.annotations.BeforeClass;
 
-@ContextConfiguration(locations={"application.xml"})
+@ContextConfiguration(locations = { "classpath*:/application.xml" })  
 public class UserServiceTestNG extends AbstractTestNGSpringContextTests {
-	@Autowired  
-    private UserService userService; 
+//	@Autowired  
+//    private UserService userService; 
+	
+	@BeforeClass  
+    public void setUp(){  
+        System.out.println("初始化");  
+    } 
 	
 	@Test
-	public void test() {
+	public void testSomething() {
 		Assert.assertEquals(1,1);
 	}
 	
