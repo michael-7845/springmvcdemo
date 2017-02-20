15598bad3904b2cbd48d590c611db03074ba762e
yukemin <yukemin@jd.com>
2017-02-20 16:05:50 +0800

    update test

--- src/test/java/com/yu/testng/UserServiceTestNG.java
+++ src/test/java/com/yu/testng/UserServiceTestNG.java
@@ -5,14 +5,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
 import org.testng.Assert;
-//import com.yu.domain.User;
-//import com.yu.service.UserService;
 import org.testng.annotations.BeforeClass;
+
+import com.yu.domain.User;
+import com.yu.service.UserService;
 
 @ContextConfiguration(locations = { "classpath*:/application.xml" })  
 public class UserServiceTestNG extends AbstractTestNGSpringContextTests {
-//	@Autowired  
-//    private UserService userService; 
+	@Autowired  
+    private UserService userService; 
 	
 	@BeforeClass  
     public void setUp(){  
@@ -24,10 +25,10 @@ public class UserServiceTestNG extends AbstractTestNGSpringContextTests {
 		Assert.assertEquals(1,1);
 	}
 	
-//	@Test  
-//    public void selectUserByIdTest(){  
-//        User user = userService.selectUserById(1);  
-//        System.out.println(user.getUserName() + ":" + user.getUserPassword());
-//        Assert.assertEquals(1,1);
-//    }  
+	@Test  
+    public void selectUserByIdTest(){  
+        User user = userService.selectUserById(1);  
+        System.out.println(user.getUserName() + ":" + user.getUserPassword());
+        Assert.assertEquals(1,1);
+    }  
 }  
